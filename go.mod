cf7644279fde3ecd73e403b3aba9cf3d7328e8ba
sabyasachi-mukherjee <mukherjee.ssachi@gmail.com>
2024-02-21 12:28:52 +0000

    backup of work till now

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sabyasachi-mukherjee/go_with_tests
+
+go 1.22.0
---
+++ sumall/sumall.go
@@ -0,0 +1,12 @@
+// fatal error with importing sum: brawl with vs code not won!
+package sumall
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
