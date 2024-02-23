5133111b8e02446f5372c33c686660ba050b5fdb
sabyasachi-mukherjee <mukherjee.ssachi@gmail.com>
2024-02-23 18:56:23 +0000

    backup of work, wallet in progress

---
+++ wallet/wallet.go
@@ -0,0 +1,48 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int // We create our own type
+
+type Stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
