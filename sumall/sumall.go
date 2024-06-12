708218929e24dacde48651ddd4d9df4f8798fb0e
sabyasachi-mukherjee <mukherjee.ssachi@gmail.com>
2024-06-12 18:42:38 +0200

    fixed issue with import

--- sumall/sumall.go
+++ sumall/sumall.go
@@ -1,12 +1,13 @@
-// fatal error with importing sum: brawl with vs code not won!
 package sumall
+
+import "example.com/gwt/sum"
 
 func SumAll(numbersToSum ...[]int) []int {
 	lengthOfNumbers := len(numbersToSum)
 	sums := make([]int, lengthOfNumbers)
 
 	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+		sums[i] = sum.Sum(numbers) // of course!
 	}
 	return sums
 }
